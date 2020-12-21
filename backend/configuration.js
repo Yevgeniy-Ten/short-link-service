0832868303293fbd678e53320b619e7ab9be0ebb
Yevgeniy Ten <foodila@hotmail.com>
2020-12-21 20:33:18 +0500

    #1 Create starter template for project

---
+++ backend/configuration.js
@@ -0,0 +1,15 @@
+const path = require("path")
+const rootPath = __dirname;
+const frontendURL = "http://localhost:3000"
+const mongoURI = "mongodb://localhost/shorter"
+const PORT = 3003
+const pagesPath = path.resolve(__dirname, "src", "pages")
+
+
+module.exports = {
+    rootPath,
+    PORT,
+    frontendURL,
+    mongoURI,
+    pagesPath
+}
---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const {PORT, frontendURL, mongoURI, pagesPath} = require("../configuration")
+
+const app = express()
+
+app.use(cors({
+    origin: frontendURL,
+    optionsSuccessStatus: 200
+}))
+app.use(express.json())
+app.use(express.static("public"))
+const start = async () => {
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.get("*", (req, res) => {
+            const pageNotFoundPath = `${pagesPath}/404.html`
+            res.status(404).sendFile(pageNotFoundPath)
+        })
+        app.listen(PORT, () => console.log(`${PORT}=>server port`))
+    } catch (e) {
+        console.log(e)
+        process.exit(1)
+    }
+}
+start()
